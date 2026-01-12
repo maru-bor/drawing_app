fad044317a8a5a716fb323ee9f43b9304fdc05b0
marie <borisova.marie1@gmail.com>
2026-01-12 18:02:18 +0100

    Added Layer class

---
+++ Layer.cs
@@ -0,0 +1,17 @@
+﻿using SkiaSharp;
+
+namespace drawing_app;
+
+public class Layer
+{
+    public string Name { get; set; }
+    public SKBitmap Bitmap { get; set; }
+    public bool Visible { get; set; } = true;
+    public float Opacity { get; set; } = 1f;
+
+    public Layer(int width, int height, string name)
+    {
+        Name = name;
+        Bitmap = new SKBitmap(width, height, true);
+    }
+}
--- obj/Debug/net10.0-windows/drawing_app.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/drawing_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("drawing_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd9fde34e1c78abb11a862fc7f0a916dbbb8c947")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64ee32810446dc5b0004accc0f40a2358dc9d200")]
 [assembly: System.Reflection.AssemblyProductAttribute("drawing_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("drawing_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
