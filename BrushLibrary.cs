3cb854816deb99c916c42048c9a38dc42bf492f0
marie <borisova.marie1@gmail.com>
2026-01-14 18:25:26 +0100

    Added BrushLibrary class

---
+++ BrushLibrary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace drawing_app;
+
+public class BrushLibrary
+{
+    public static ObservableCollection<BrushPreset> DefaultBrushes { get; } =
+        new ObservableCollection<BrushPreset>
+        {
+            new BrushPreset("Pencil", 2f, 255, 0.6f),
+            new BrushPreset("Ink Pen", 5f, 255, 0.25f),
+            new BrushPreset("Soft Brush", 20f, 80, 0.15f),
+            new BrushPreset("Marker", 15f, 180, 0.35f),
+            new BrushPreset("Eraser", 20f, 255, 0.25f) { IsEraser = true }
+        };
+}
